9f206c82f4f7600483332330511526e90d4f3355
Jason <152582224+JEX2009@users.noreply.github.com>
2025-05-13 13:30:49 -0600

    Se completa la funcionalidad de ingresar

--- bancoV2/menu/urls.py
+++ bancoV2/menu/urls.py
@@ -2,16 +2,16 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.start, name="start"),
-    path('ingresar/', views.ingresarTodos, name="ingresarTodos"),
-    path('retirar/', views.retirar, name="retirar"),
-    path('rConfirmacion/', views.retirarConfirmacion, name="retirarConfirmacion"),
-    path('sobres/', views.sobres, name="sobres"),
-    path('obtencionId/<int:id>', views.obtencionId, name="obtencionId"),
-    path('sobresModificar/<int:id>', views.formModificar, name="sobresModificar"),
-    path('sobresGuardar/<int:id>', views.guardarSobre, name="sobresGuardar"),
-    path('sobresEliminar/<int:id>', views.sobresEliminar, name="sobresEliminar"),
-    path('crearSobre/', views.formCrearSobre, name="crearSobre"),
-    path('sobreCreado/', views.guardarNuevo, name="sobreCreado"),
-    path('ingresarTodo/', views.ingresar, name="ingresarParaTodos")
+    path('', views.start, name="start"),# Barra lateral puesto 1
+    path('ingresar/', views.ingresarTodos, name="ingresarTodos"),#Barra lateral puesto 2
+    path('retirar/', views.retirar, name="retirar"),#Barra lateral puesto 3
+    path('sobres/', views.sobres, name="sobres"),#Barra lateral puesto 4
+    path('rConfirmacion/', views.retirarConfirmacion, name="retirarConfirmacion"),# Paso de de ingresar
+    path('obtencionId/<int:id>', views.obtencionId, name="obtencionId"),# Se usa en modificar
+    path('sobresModificar/<int:id>', views.formModificar, name="sobresModificar"),#Se usa en modificar para obtener
+    path('sobresGuardar/<int:id>', views.guardarSobre, name="sobresGuardar"),#Se usa en modificar para guardar sus cambios
+    path('sobresEliminar/<int:id>', views.sobresEliminar, name="sobresEliminar"),#Se usa en modificar para eliminar 
+    path('crearSobre/', views.formCrearSobre, name="crearSobre"),#Se usa en modificar para crear sobre
+    path('sobreCreado/', views.guardarNuevo, name="sobreCreado"),# Se usa en crear para guardar
+    path('ingresarTodo/', views.ingresar, name="ingresarParaTodos")#Se usa para procesar los datos
 ]
--- bancoV2/menu/views.py
+++ bancoV2/menu/views.py
@@ -87,37 +87,48 @@ def sobresEliminar(request,id):
     return HttpResponse("Se han realizado los cambio de forma satisfactoria <br> <a href='/'>Salir</a>")
 
 def ingresar(request):
+    sobreLleno = []
     cantidad = []
-    total = 0
-    sobres= sobreModels.objects.all()
+    sobresVacios = []
     colones = [1000,2000,5000,10000,20000]
     
     for billetes in request.POST:
         cantidad.append(request.POST.get(billetes))
     cantidad.pop(0)
     
-    for indice in range(len(colones)):
-        total += colones[indice] *int(cantidad[indice])
+    total = sum(col * int(cant) for col, cant in zip(colones, cantidad)) if cantidad else 0
     
-    sobreLleno = []
-    
+    sobreMayorPorcentaje = sobreModels("Prueba",0,0,0,0)
     for sobre in sobreModels.objects.all():
+        if sobreMayorPorcentaje.porcentaje <sobre.porcentaje:
+            sobreMayorPorcentaje = sobre
+            
         if sobre.limite == sobre.saldo and sobre.saldo > 0:
             sobreLleno.append(sobre)
-    restante = 0
-    for sobre in sobreModels.objects.all():
-        if sobre not in sobreLleno:
-            dinero = sobre.porcentaje/100 * total
-            print("dinero: " ,dinero)
-            rebajo = floor((dinero / 1000))*1000
-            restante += dinero - rebajo
-            print("restante",restante)
-            if rebajo < 1000 and rebajo + restante >= 1000:
-                rebajo += restante
-            rebajo = int(rebajo)
-            print("rebajo:" , rebajo)
-            sobre.saldo += rebajo
-            sobre.save()
+        else:
+            sobresVacios.append(sobre)
+            
+    dineroSale = total
+    for sobre in sobresVacios:
+        porcentaje = sobre.porcentaje / 100     #Calculan cuanto dinero ira para ese sobre
+        dineroAsignado = int(total * porcentaje)#Calculan cuanto dinero ira para ese sobre
+        if dineroAsignado % 1000 != 0  and dineroSale >= 1000:
+            while dineroAsignado % 1000 != 0 :
+                dineroAsignado -=100
+            #Si en dinero asignado empieza a dar menos que 1000 se meten al ultimo
+        print(dineroAsignado)
+        if dineroAsignado > dineroSale:
+            dineroAsignado = dineroSale
+            #Si dinero saliente es menor de lo que se calcula se pone el restante
 
+        sobre.saldo += dineroAsignado
+        sobre.save()
+        dineroSale -= dineroAsignado
+        #Proceso de guardado en sobre
+        
+    if dineroSale > 0 and sobresVacios:
+        sobreMayorPorcentaje.saldo += dineroSale
+        sobreMayorPorcentaje.save()
+        #Se guarda lo resatante 
     return render(request, 'ingresar/ingreso.html',{
-        "sobres":sobres})
+        "sobres":sobresVacios})
