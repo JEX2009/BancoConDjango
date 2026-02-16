eaeef391590810c61240bb411b6354b03eca4868
Jason <152582224+JEX2009@users.noreply.github.com>
2026-02-15 21:18:49 -0600

    feat Initializing docker

--- bancoV2/bancoV2/settings.py
+++ bancoV2/bancoV2/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'bancoV2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME', 'banco_db'),
+        'USER': os.environ.get('DATABASE_USER', 'usuario_banco'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'password_seguro'),
+        'HOST': os.environ.get('DATABASE_HOST', 'db'),
+        'PORT': os.environ.get('DATABASE_PORT', '5432'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
